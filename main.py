f73555936ebddd6fa03938c7d9865f1b3cf6cfdb
igor <igorcampos064@gmail.com>
2023-07-26 18:29:59 -0300

    Finalizado o scritp do Python

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+endpoint = FastAPI()
+
+usuarios = [
+    {"id": 1, "nome": "Igor"},
+    {"id": 2, "nome": "Ketly"}
+]
+
+grupos = [
+    {"id": 1, "nome": "Admin"},
+    {"id": 1, "nome": "Usuarios"}
+]
+
+@endpoint.get("/grupos", grupos)
+async def get_grupos():
+    return grupos
+
+@endpoint.get("/usuarios" usuarios)
+async def get_users():
+    return usuarios
