0ddc2c8872eb22000a54e805591d3efd4824549c
igor <igorcampos064@gmail.com>
2023-07-27 18:08:11 -0300

    Adição da conexão ao banco de dados e a busca dos valores 'usuarios' e 'grupos' dentro do mesmo

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI
 from typing import List
+import pymongo
 
+#Diretorio do log
+log = 'D:/DEV/ProjetoEndpoints/endpoints.log'
+
+#criando uma instância do objeto Framework FastAPI
 endpoint = FastAPI()
 
-usuarios = [
-    {"id": 1, "nome": "Igor"},
-    {"id": 2, "nome": "Ketly"},
-]
+#Conexão do banco MONGODB
+c = "mongodb+srv://IgorAraujo:Iaraujo2020@mongodb.a46kimi.mongodb.net/?retryWrites=true&w=majority"
 
-grupos = [
-    {"id": 1, "nome": "Admin"},
-    {"id": 2, "nome": "Usuarios"},
-]
+try:
+    mongo_client = pymongo.MongoClient(c)
+    db = mongo_client["fortics"]
+    usuarios_collection = db["usuarios"]
+    grupos_collection = db["grupos"]
+except pymongo.errors.ConnectionFailure:
+    log("Não foi possível conectar ao MongoDB."+str(pymongo.errors.ConnectionFailure), log)
 
+#Endpoints
 @endpoint.get("/grupos", response_model=List[dict])
 async def get_grupos():
+    grupos = list(grupos_collection.find())
     return grupos
 
 @endpoint.get("/usuarios", response_model=List[dict])
 async def get_usuarios():
+    usuarios = list(usuarios_collection.find())
     return usuarios
+
+
