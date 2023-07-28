8bf712cb4543a916d4d9b6d2ccb5c14898a1c0e9
igor <igorcampos064@gmail.com>
2023-07-28 12:55:59 -0300

    adicionando variaveis de ambiente

--- functions.py
+++
@@ -1,8 +0,0 @@
-import time
-
-#Funçao para logs do sistema
-def log(mensagem, log):
-    arquivo = open(log, 'a')
-    data = str(time.strftime("%Y-%m-%d %H:%M:%S"))
-    arquivo.write('['+data+'] '+mensagem+'\n\n')
-    arquivo.close()
---
+++ varivaveisAmbiente.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DB_USER'] = 'fortics'
+os.environ['DB_PASSWORD'] = 'nXyjK0kp8Uyxs4Xv'
+os.environ['DB_HOST'] = 'mongodb.a46kimi.mongodb.net'
