d1be3bdf0475ae3169ad3f4d0798793a31fc3af7
igor <igorcampos064@gmail.com>
2023-07-28 16:13:13 -0300

    Ajustado o log, para que não gere arquivo e mande para o log do proprio docker

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+import os
 from fastapi import FastAPI
 from typing import List
 import pymongo
+from varivaveisAmbiente import *
+import logging
 
-#Diretorio do log
-log = 'D:/DEV/ProjetoEndpoints/endpoints.log'
+#log
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('log_main')
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
 
 #criando uma instância do objeto Framework FastAPI
 endpoint = FastAPI()
 
 #Conexão do banco MONGODB
-c = "mongodb+srv://fortics:nXyjK0kp8Uyxs4Xv@mongodb.a46kimi.mongodb.net/?retryWrites=true&w=majority"
+c = os.getenv('DB_PROTOCOL')+ "://" + os.getenv('DB_USER') + ":" + os.getenv('DB_PASSWORD') + "@" +os.getenv('DB_HOST')+ "/?retryWrites=true&w=majority"
 
 try:
-    mongo_client = pymongo.MongoClient(c)
+    mongo_client = pymongo.MongoClient(c, serverSelectionTimeoutMS=5000)
+    mongo_client.server_info()
     db = mongo_client["fortics"]
     usuarios_collection = db["usuarios"]
     grupos_collection = db["grupos"]
+    logging.debug("Banco conectado com sucesso")
 except pymongo.errors.ConnectionFailure:
-    log("Não foi possível conectar ao MongoDB."+str(pymongo.errors.ConnectionFailure), log)
+    logging.error("Não foi possível conectar ao MongoDB.")
 
 #Endpoints
 @endpoint.get("/grupos", response_model=List[dict])
--- varivaveisAmbiente.py
+++ varivaveisAmbiente.py
@@ -1,5 +1,6 @@
 import os
 
+os.environ['DB_PROTOCOL'] = 'mongodb+srv'
 os.environ['DB_USER'] = 'fortics'
 os.environ['DB_PASSWORD'] = 'nXyjK0kp8Uyxs4Xv'
 os.environ['DB_HOST'] = 'mongodb.a46kimi.mongodb.net'
